1d5d2e77d87f916c79a459b20deb8a8c0a238bc4
Mohammed Al-Baqer <mohmmadkasper@gmail.com>
2025-08-20 01:19:15 +0300

    Update mylib.pyx

--- mylib.pyx
+++ mylib.pyx
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 
+# Developer : Mohammed Al-Baqer
+
 cdef public int add_numbers(int a, int b):
     return a + b
 
 def py_add_numbers(a, b):
     return add_numbers(a, b)
+
